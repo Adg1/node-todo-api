cef7a2c9350375241272ef89729e6a262617cc02
Adg1 <amandeepgupta1@gmail.com>
2017-03-06 13:49:51 +0530

    [BUG FIXED 2]Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
-var {MONGODB_URI} = require('./config')
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
