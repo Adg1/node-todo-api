b552b5f3c305dc7cbb8d1fa8d183370290a6bcd0
Adg1 <amandeepgupta1@gmail.com>
2017-03-06 08:35:27 +0530

    Setup app for heroku

---
+++ server/db/config.js
@@ -0,0 +1,6 @@
+const MONGODB_URI
+  = "mongodb://amandeep:amandeep@ds119370.mlab.com:19370/node-todo-api";
+
+module.exports = {
+  MONGODB_URI
+};
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+var {MONGODB_URI} = require('./config')
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
