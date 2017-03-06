15aebfa526f97d4bc1b7d42d70a33d205e767195
Adg1 <amandeepgupta1@gmail.com>
2017-03-06 08:57:14 +0530

    [Bug Fixed 2]Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var {MONGODB_URI} = require('./config')
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+if(!process.env.PORT) {
+  MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}
+mongoose.connect( MONGODB_URI);
 
 module.exports = {
   mongoose
